cdd5be551c703cc3e6389d3908c59bd4eb30af9e
Caique Dutra Santos <caique.santos@ibm.com>
2025-10-08 11:33:03 -0300

    fixing workspace config

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
-module "powervs_infrastructure" {
-  source      = "terraform-ibm-modules/powervs-workspace/ibm"
-  version = "latest"
+module "powervs-workspace" {
+  source  = "terraform-ibm-modules/powervs-workspace/ibm"
+  version = "3.2.1"
   pi_workspace_name = var.pi_workspace_name
   pi_zone = var.pi_resource_group_name
   pi_ssh_public_key = var.pi_ssh_public_key
