331f55a0a12d991c66c25bba2549ad9908864d16
Caique Dutra Santos <caique.santos@ibm.com>
2025-10-08 10:34:15 -0300

    adding workspace config

--- main.tf
+++ main.tf
@@ -1,7 +1,8 @@
-module "deploy-arch-ibm-pvs-inf" {
-  source      = "terraform-ibm-modules/powervs-infrastructure/ibm"
-  prefix                     = var.pi_workspace_name
-  powervs_resource_group_name = var.pi_resource_group_name
-  powervs_zone                = var.pi_zone
-  ssh_public_key              = var.pi_ssh_public_key
+module "powervs_infrastructure" {
+  source      = "terraform-ibm-modules/powervs-workspace/ibm"
+  version = "latest"
+  pi_workspace_name = var.pi_workspace_name
+  pi_zone = var.pi_resource_group_name
+  pi_ssh_public_key = var.pi_ssh_public_key
+  pi_public_subnet_enable = false
 }
