a9a6b7a06e96871e296ea67130ca28cefd9befb3
Caique Dutra Santos <caique.santos@ibm.com>
2025-10-08 10:06:59 -0300

    v2

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "deploy-arch-ibm-pvs-inf" {
-  source      = "https://cm.globalcatalog.cloud.ibm.com/api/v1-beta/offering/source/archive//solutions/standard-plus-vsi?archive=tgz&flavor=standard-plus-vsi&installType=fullstack&kind=terraform&name=deploy-arch-ibm-pvs-inf&version=v9.0.1"
+  source      = "terraform-ibm-modules/powervs-infrastructure/ibm"
   prefix                     = var.pi_workspace_name
   powervs_resource_group_name = var.pi_resource_group_name
   powervs_zone                = var.pi_zone
