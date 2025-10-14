496e5c3fb61dd2aa0f4a9bbc05a68c2b635d9a08
Caique Dutra Santos <caique.santos@ibm.com>
2025-10-14 12:40:31 -0300

    adjusting provider hashicorp

--- main.tf
+++ main.tf
@@ -1,7 +1,21 @@
+terraform {
+  required_providers {
+    ibm = {
+      source  = "ibm-cloud/ibm"
+      version = ">= 1.81.0"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = ">= 0.9.1"
+    }
+  }
+}
+
 provider "ibm" {
   region = "sao01"   # exemplo: "sao01" ou "us-south"
   zone   = var.pi_zone     # exemplo: "sao01-1"
 }
+
 
 module "powervs-workspace" {
   source  = "terraform-ibm-modules/powervs-workspace/ibm"
