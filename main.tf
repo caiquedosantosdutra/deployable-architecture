6fa233240ee8f8af4fd172b971fec527f2257265
Caique Dutra Santos <caique.santos@ibm.com>
2025-10-14 11:55:51 -0300

    adjusting vars

--- main.tf
+++ main.tf
@@ -2,7 +2,8 @@ module "powervs-workspace" {
   source  = "terraform-ibm-modules/powervs-workspace/ibm"
   version = "3.2.1"
   pi_workspace_name = var.pi_workspace_name
-  pi_zone = var.pi_resource_group_name
+  pi_resource_group_name = var.pi_resource_group_name
+  pi_zone = var.pi_zone
   pi_ssh_public_key = var.pi_ssh_public_key
   pi_public_subnet_enable = false
 }
