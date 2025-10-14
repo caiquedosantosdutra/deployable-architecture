0d610c210d37e8a1f3cbfd49c56043b29f40d4a1
Caique Dutra Santos <caique.santos@ibm.com>
2025-10-14 12:35:51 -0300

    adjusting provider ibm

--- main.tf
+++ main.tf
@@ -1,14 +1,6 @@
-terraform {
-  required_providers {
-    ibm = {
-      source  = "ibm-cloud/ibm"
-      version = ">= 1.81.0"
-    }
-    time = {
-      source  = "hashicorp/time"
-      version = ">= 0.9.1"
-    }
-  }
+provider "ibm" {
+  region = "sao01"   # exemplo: "sao01" ou "us-south"
+  zone   = var.pi_zone     # exemplo: "sao01-1"
 }
 
 module "powervs-workspace" {
@@ -17,7 +9,7 @@ module "powervs-workspace" {
   pi_workspace_name = var.pi_workspace_name
   pi_resource_group_name = var.pi_resource_group_name
   pi_zone = var.pi_zone
-  pi_ssh_public_key = var.pi_ssh_public_key
+  pi_ssh_public_key = var.pi_ssh_public_key.value
   pi_public_subnet_enable = false
 }
 
