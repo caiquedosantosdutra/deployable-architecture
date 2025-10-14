73d9cefef367769a6c5d99510345b53682fafc74
Caique Dutra Santos <caique.santos@ibm.com>
2025-10-14 14:27:00 -0300

    v3

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ module "powervs-workspace" {
   pi_workspace_name = var.pi_workspace_name
   pi_resource_group_name = var.pi_resource_group_name
   pi_zone = var.pi_zone
-  pi_ssh_public_key = var.pi_ssh_public_key.value
+  pi_ssh_public_key = var.pi_ssh_public_key
   pi_public_subnet_enable = false
 }
 
